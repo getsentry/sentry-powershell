cde48928c50e36b785711af6e452a366f5a50210
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-02-10 21:48:15 +0100

    find failing tests

--- tests/init.tests.ps1
+++ tests/init.tests.ps1
@@ -17,33 +17,33 @@ Describe 'SentrySdk' {
         [Sentry.SentrySdk]::IsEnabled | Should -Be $true
     }
 
-    It 'close closes the SDK' {
-        $handle = [Sentry.SentrySdk]::init('https://key@host/1')
-        $handle | Should -Be Sentry.SentrySdk+DisposeHandle
-        [Sentry.SentrySdk]::IsEnabled | Should -Be $true
-        [Sentry.SentrySdk]::close()
-        [Sentry.SentrySdk]::IsEnabled | Should -Be $false
-    }
+    # It 'close closes the SDK' {
+    #     $handle = [Sentry.SentrySdk]::init('https://key@host/1')
+    #     $handle | Should -Be Sentry.SentrySdk+DisposeHandle
+    #     [Sentry.SentrySdk]::IsEnabled | Should -Be $true
+    #     [Sentry.SentrySdk]::close()
+    #     [Sentry.SentrySdk]::IsEnabled | Should -Be $false
+    # }
 
-    It 'init respects options' {
-        class TestLogger:Sentry.Infrastructure.DiagnosticLogger
-        {
-            TestLogger([Sentry.SentryLevel]$level) : base($level) {}
+    # It 'init respects options' {
+    #     class TestLogger:Sentry.Infrastructure.DiagnosticLogger
+    #     {
+    #         TestLogger([Sentry.SentryLevel]$level) : base($level) {}
 
-            $entries = [System.Collections.Concurrent.ConcurrentQueue[string]]::new();
+    #         $entries = [System.Collections.Concurrent.ConcurrentQueue[string]]::new();
 
-            [void]LogMessage([string] $message) { $this.entries.Enqueue($message); }
-        }
+    #         [void]LogMessage([string] $message) { $this.entries.Enqueue($message); }
+    #     }
 
-        $logger = [TestLogger]::new([Sentry.SentryLevel]::Debug)
+    #     $logger = [TestLogger]::new([Sentry.SentryLevel]::Debug)
 
-        $options = [Sentry.SentryOptions]::new()
-        $options.Debug = $true
-        $options.Dsn = 'https://key@127.0.0.1/1'
-        $options.DiagnosticLogger = $logger
-        [Sentry.SentrySdk]::init($options)
-        [Sentry.SentrySdk]::IsEnabled | Should -Be $true
+    #     $options = [Sentry.SentryOptions]::new()
+    #     $options.Debug = $true
+    #     $options.Dsn = 'https://key@127.0.0.1/1'
+    #     $options.DiagnosticLogger = $logger
+    #     [Sentry.SentrySdk]::init($options)
+    #     [Sentry.SentrySdk]::IsEnabled | Should -Be $true
 
-        $logger.entries.Count | Should -BeGreaterThan 0
-    }
+    #     $logger.entries.Count | Should -BeGreaterThan 0
+    # }
 }
