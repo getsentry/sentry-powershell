f9d5064e1660f5e4a7918a9731e405cb396f48f5
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-02-15 21:34:30 +0100

    ignore compiler warnings with comment

--- module/Sentry.psm1
+++ module/Sentry.psm1
@@ -4,7 +4,11 @@ $privateDir = Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) 'priva
 . "$privateDir/Get-SentryAssembliesDirectory.ps1"
 $sentryDllPath = (Join-Path (Get-SentryAssembliesDirectory) 'Sentry.dll')
 
-Add-Type -TypeDefinition (Get-Content "$privateDir/SentryEventProcessor.cs" -Raw) -ReferencedAssemblies $sentryDllPath -IgnoreWarnings
+# On PowerShell 7.3, we need to ignore a warning about using .NET 6 Sentry.assembly (that targets System.Runtime 6.0.0)
+# while we actually target System.Runtime 7.0.0. The problem is no .NET7 version of Sentry anymore.
+$ignoreCsCompilerWarnings = ($PSVersionTable.PSVersion.Major -eq 7 -and $PSVersionTable.PSVersion.Minor -eq 3)
+Add-Type -TypeDefinition (Get-Content "$privateDir/SentryEventProcessor.cs" -Raw) -ReferencedAssemblies $sentryDllPath -IgnoreWarnings:$ignoreCsCompilerWarnings
+
 
 . "$publicDir/Out-Sentry.ps1"
 . "$publicDir/Invoke-WithSentry.ps1"
