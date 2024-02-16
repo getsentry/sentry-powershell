5c617aea0bab51822623f01092613dd17694966e
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-02-16 12:58:18 +0100

    expose packages based on stack trace

---
+++ tests/packages.tests.ps1
@@ -0,0 +1,57 @@
+BeforeAll {
+    . "$PSScriptRoot/utils.ps1"
+    $events = [System.Collections.Generic.List[Sentry.SentryEvent]]::new();
+    StartSentryForEventTests ([ref] $events)
+}
+
+AfterAll {
+    Stop-Sentry
+}
+
+Describe 'Out-Sentry for <_>' -ForEach @('message', 'error') {
+    BeforeEach {
+        $param = $_
+        if ($param -eq 'error')
+        {
+            try
+            {
+                throw 'error'
+            }
+            catch
+            {
+                $_ | Out-Sentry
+            }
+        }
+        else
+        {
+            $param | Out-Sentry
+        }
+        $events.Count | Should -Be 1
+        [Sentry.SentryEvent]$event = $events.ToArray()[0]
+    }
+
+    AfterEach {
+        $events.Clear()
+    }
+
+    It 'Sets SDK info' {
+        $event.Sdk.Name | Should -Be 'sentry.powershell'
+        $event.Sdk.Version | Should -Match '^\d+\.\d+\.\d+$'
+    }
+
+    It 'Sets .NET SDK as a package' {
+        $package = $event.Sdk.Packages | Where-Object -Property Name -EQ 'nuget:sentry.dotnet'
+        $package.Version | Should -Match '^\d+\.\d+\.\d+$'
+    }
+
+    It 'Sets PowerShell SDK as a package' {
+        $package = $event.Sdk.Packages | Where-Object -Property Name -EQ 'ps:Sentry'
+        $package.Version | Should -Match $event.Sdk.Version
+    }
+
+    It 'Sets PS modules as packages' {
+        $pesterModule = Get-Module -Name 'Pester'
+        $package = $event.Sdk.Packages | Where-Object -Property Name -EQ "ps:$($pesterModule.Name)"
+        $package.Version | Should -Be $pesterModule.Version.ToString()
+    }
+}
