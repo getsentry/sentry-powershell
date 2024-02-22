611742fd72481d90915778d4888b0880f0795cc2
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-02-22 20:45:29 +0100

    fix windows powershell

--- module/private/DiagnosticLogger.ps1
+++ module/private/DiagnosticLogger.ps1
@@ -33,28 +33,28 @@ class DiagnosticLogger : Sentry.Extensibility.IDiagnosticLogger
 
         switch ($level)
         {
-            ([Sentry.SentryLevel]::Debug)
-            {
-                Write-Debug $message -Debug:$true
-            }
             ([Sentry.SentryLevel]::Info)
             {
-                Write-Verbose $message -Verbose:$true
+                Write-Verbose $message
             }
             ([Sentry.SentryLevel]::Warning)
             {
-                Write-Warning $message -WarningAction:Continue
+                Write-Warning $message
             }
             ([Sentry.SentryLevel]::Error)
             {
-                Write-Error $message -ErrorAction:Continue
+                Write-Error $message
             }
             ([Sentry.SentryLevel]::Fatal)
             {
-                Write-Error $message -ErrorAction:Continue
+                Write-Error $message
             }
             default
             {
+                # Workaround for Windows Powershell issue of halting and asking for user confirmation.
+                # see https://github.com/PowerShell/PowerShell/issues/5148
+                $DebugPreference = 'Continue'
+
                 Write-Debug $message
             }
         }
