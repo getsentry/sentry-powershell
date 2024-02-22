db5a90d160341058fcb8fcca91627ae2b49435d7
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-02-22 09:46:09 +0100

    feat: Add-SentryBreadcrumb

---
+++ module/public/Add-SentryBreadcrumb.ps1
@@ -0,0 +1,28 @@
+function Add-SentryBreadcrumb
+{
+    param(
+        [Parameter(Mandatory, ValueFromPipeline = $true)]
+        [string] $Message,
+
+        [string] $Category = $null,
+        [string] $Type = $null,
+        [hashtable] $Data = $null,
+        [Sentry.BreadcrumbLevel] $Level = [Sentry.BreadcrumbLevel]::Info)
+
+    begin
+    {
+        if ($null -eq $Data)
+        {
+            $DataDict = $null
+        }
+        else
+        {
+            $DataDict = [System.Collections.Generic.Dictionary[string, string]]::new()
+            $Data.Keys | ForEach-Object { $DataDict.Add($_, $Data[$_]) }
+        }
+    }
+    process
+    {
+        [Sentry.SentrySdk]::AddBreadcrumb($Message, $Category, $Type, $DataDict, $Level)
+    }
+}
---
+++ tests/breadcrumbs.tests.ps1
@@ -0,0 +1,46 @@
+BeforeAll {
+    . "$PSScriptRoot/utils.ps1"
+}
+
+Describe 'Add-SentryBreadcrumb' {
+    BeforeEach {
+        $events = [System.Collections.Generic.List[Sentry.SentryEvent]]::new();
+        $transport = [RecordingTransport]::new()
+        StartSentryForEventTests ([ref] $events) ([ref] $transport)
+    }
+
+    AfterEach {
+        $events.Clear()
+        Stop-Sentry
+    }
+
+    It 'Pipes a message' {
+        'hello there' | Add-SentryBreadcrumb
+        'msg' | Out-Sentry
+
+        $events[0].Breadcrumbs.Count | Should -Be 1
+        $events[0].Breadcrumbs[0].Message | Should -Be 'hello there'
+        $events[0].Breadcrumbs[0].Data | Should -Be $null
+    }
+
+    It 'Adds data' {
+        'hello there' | Add-SentryBreadcrumb -Data @{ 'key' = 'value' }
+        'msg' | Out-Sentry
+
+        $events[0].Breadcrumbs.Count | Should -Be 1
+        $events[0].Breadcrumbs[0].Message | Should -Be 'hello there'
+        $events[0].Breadcrumbs[0].Data['key'] | Should -Be 'value'
+    }
+
+    It 'Passes all args' {
+        Add-SentryBreadcrumb -Message 'hello there' -Category 'cat' -Type 'foo' -Level Warning -Data @{ 'key' = 'value' }
+        'msg' | Out-Sentry
+
+        $events[0].Breadcrumbs.Count | Should -Be 1
+        $events[0].Breadcrumbs[0].Message | Should -Be 'hello there'
+        $events[0].Breadcrumbs[0].Level | Should -Be ([Sentry.BreadcrumbLevel]::Warning)
+        $events[0].Breadcrumbs[0].Category | Should -Be 'cat'
+        $events[0].Breadcrumbs[0].Type | Should -Be 'foo'
+        $events[0].Breadcrumbs[0].Data['key'] | Should -Be 'value'
+    }
+}
