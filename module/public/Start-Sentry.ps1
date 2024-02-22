244fff417fc91b30435159e88a9fadc4b741efa4
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2024-02-22 20:47:12 +0100

    feat: diagnostic logger (#29)
    * feat: diagnostic logger
    
    * fix Ci
    
    * fix logging
    
    * fix windows powershell

--- module/public/Start-Sentry.ps1
+++ module/public/Start-Sentry.ps1
@@ -1,3 +1,4 @@
+. "$privateDir/DiagnosticLogger.ps1"
 . "$privateDir/ScopeIntegration.ps1"
 . "$privateDir/EventUpdater.ps1"
 
@@ -20,9 +21,15 @@ function Start-Sentry
         [Sentry.sentryOptionsExtensions]::AddIntegration($options, [ScopeIntegration]::new())
         [Sentry.sentryOptionsExtensions]::AddEventProcessor($options, [EventUpdater]::new())
 
-        if ($DebugPreference -ne 'SilentlyContinue')
+        if ($DebugPreference -eq 'SilentlyContinue')
+        {
+            $Options.Debug = $false
+            $options.DiagnosticLevel = [Sentry.SentryLevel]::Info
+        }
+        else
         {
             $Options.Debug = $true
+            $options.DiagnosticLevel = [Sentry.SentryLevel]::Debug
         }
 
         if ($EditOptions -eq $null)
@@ -34,9 +41,15 @@ function Start-Sentry
             # Execute the script block in the caller's scope & set the automatic $_ variable to the options object.
             $options | ForEach-Object $EditOptions
         }
+
+        $options.DiagnosticLogger = [DiagnosticLogger]::new($options.DiagnosticLevel)
+
+        # Workaround for https://github.com/getsentry/sentry-dotnet/issues/3141
+        [Sentry.SentryOptionsExtensions]::DisableAppDomainProcessExitFlush($options)
     }
     process
     {
         [Sentry.SentrySdk]::init($options) | Out-Null
     }
 }
+
