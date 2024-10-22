045d74e9d2d28a077d938b7477e09eef3c803461
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2024-10-22 18:43:21 +0200

    test: integration test improvement (#64)
    * update tests
    
    * remove empty lines

--- tests/integration-test-script.ps1
+++ tests/integration-test-script.ps1
@@ -4,6 +4,7 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Import-Module ../modules/Sentry/Sentry.psd1
 . ./utils.ps1
+. ./throwingshort.ps1
 
 $events = [System.Collections.Generic.List[Sentry.SentryEvent]]::new();
 $transport = [RecordingTransport]::new()
@@ -11,20 +12,24 @@ StartSentryForEventTests ([ref] $events) ([ref] $transport)
 
 try
 {
-    funcA 'throw' 'error'
+    funcC
 }
 catch
 {
     $_ | Out-Sentry | Out-Null
 }
 
-$events[0].SentryThreads.Stacktrace.Frames | ForEach-Object {
+$thread = $events[0].SentryThreads | Where-Object { $_.Id -eq 0 }
+$thread.Stacktrace.Frames | ForEach-Object {
     '----------------' | Out-String
     $frame = $_
     $properties = $frame | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name
     foreach ($prop in $properties)
     {
         $value = $frame.$prop | Out-String -Width 500
-        "$($prop): $value"
+        if ("$value" -ne '')
+        {
+            "$($prop): $value"
+        }
     }
 }
