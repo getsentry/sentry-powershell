593e79f1fb7521dbca5db6f12dc01befc562a9b0
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2024-10-22 17:43:25 +0200

    add out-sentry integration-test script (#63)
    * add out-sentry integration-test script
    
    * improve test failure output
    
    * fix tests on windows-powershell
    
    * set explicit width
    
    * change integration test to manual formatting
    
    * go back to colons because of windows powershell
    
    * try to appease CI
    
    * more test fixes and adding piped command test
    
    * fixing ci
    
    * fix macos/linux CI

---
+++ tests/integration-test-script.ps1
@@ -0,0 +1,30 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
+Import-Module ../modules/Sentry/Sentry.psd1
+. ./utils.ps1
+
+$events = [System.Collections.Generic.List[Sentry.SentryEvent]]::new();
+$transport = [RecordingTransport]::new()
+StartSentryForEventTests ([ref] $events) ([ref] $transport)
+
+try
+{
+    funcA 'throw' 'error'
+}
+catch
+{
+    $_ | Out-Sentry | Out-Null
+}
+
+$events[0].SentryThreads.Stacktrace.Frames | ForEach-Object {
+    '----------------' | Out-String
+    $frame = $_
+    $properties = $frame | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name
+    foreach ($prop in $properties)
+    {
+        $value = $frame.$prop | Out-String -Width 500
+        "$($prop): $value"
+    }
+}
---
+++ tests/integration.tests.ps1
@@ -0,0 +1,254 @@
+BeforeAll {
+    $integrationTestScript = "$PSScriptRoot$([IO.Path]::DirectorySeparatorChar)integration-test-script.ps1"
+    $integrationTestScriptContent = Get-Content -Raw $integrationTestScript
+
+    $checkOutput = {
+        param([string[]] $output, [string[]] $expected)
+        # Remove color codes
+        $output = $output -replace '\x1b\[[0-9;]*[a-z]', ''
+
+        # Remove escape sequences (these appear on macOS and Linux)
+        $output = $output -replace '\e\[\?1[hl]', ''
+
+        # Remove warnings
+        $output = $output | Where-Object { $_ -notmatch 'WARNING: warning CS1701: Assuming assembly reference' }
+
+        # Remove empty lines
+        $output = $output | Where-Object { $_ -ne '' }
+
+        # Print out so that we can compare the whole output if the test fails
+        $output | Write-Host
+
+        for ($i = 0; $i -lt $expected.Count -and $i -lt $output.Count; $i++)
+        {
+            $output[$i] | Should -Be $expected[$i] -Because "Output line $i"
+        }
+        $output.Count | Should -Be $expected.Count
+    }
+}
+
+# These fail when THIS script is executed on Windows PowerShell 5.1 or Poweshell 7.3 or lower with the following error:
+# ParserError:
+# Line |
+#   28 |          $($prop): $value
+#      |                  ~
+#      | Unexpected token ':' in expression or statement.
+#
+# It looks like the variable `$integrationTestScriptContent` is expanded in place and then evaluted as an expression.
+# Let's just skip these versions. We test Windows PowerShell as the target anyway in a test case.
+# And we can live without testing on PowerShell 7.2 & 7.3 because we have tests for 7.4.
+Describe 'Out-Sentry captures expected stack traces for command argument' -Skip:(($PSVersionTable.PSVersion.Major -eq 7 -and $PSVersionTable.PSVersion.Minor -le 3) -or $PSVersionTable.PSEdition -eq 'Desktop') {
+    BeforeEach {
+        Push-Location "$PSScriptRoot"
+        $expected = @(
+            '----------------'
+            'AbsolutePath: <No file>'
+            'AddressMode: '
+            'ColumnNumber: '
+            'ContextLine: '
+            'FileName: '
+            'FramesOmitted: '
+            'Function: <ScriptBlock>'
+            'FunctionId: '
+            'ImageAddress: '
+            'InApp: True'
+            'InstructionAddress: '
+            'LineNumber: 1'
+            'Module: '
+            'Package: '
+            'Platform: '
+            'PostContext: '
+            'PreContext: '
+            'SymbolAddress: '
+            'Vars: '
+            '----------------'
+            'AbsolutePath: <No file>'
+            'AddressMode: '
+            'ColumnNumber: '
+            'ContextLine: '
+            'FileName: '
+            'FramesOmitted: '
+            'Function: <ScriptBlock>'
+            'FunctionId: '
+            'ImageAddress: '
+            'InApp: True'
+            'InstructionAddress: '
+            'LineNumber: 14'
+            'Module: '
+            'Package: '
+            'Platform: '
+            'PostContext: '
+            'PreContext: '
+            'SymbolAddress: '
+            'Vars: '
+            '----------------'
+            'AbsolutePath: '
+            'AddressMode: '
+            'ColumnNumber: 5'
+            "ContextLine:     funcA 'throw' 'error'"
+            'FileName: '
+            'FramesOmitted: '
+            'Function: '
+            'FunctionId: '
+            'ImageAddress: '
+            'InApp: True'
+            'InstructionAddress: '
+            'LineNumber: 14'
+            'Module: '
+            'Package: '
+            'Platform: '
+            'PostContext: '
+            'PreContext: '
+            'SymbolAddress: '
+            'Vars: '
+        )
+
+    }
+
+    AfterEach {
+        Pop-Location
+    }
+
+    It 'Windows PowerShell' -Skip:($env:OS -ne 'Windows_NT') {
+        $output = powershell.exe -Command "& {$integrationTestScriptContent}" -ErrorAction Continue
+        $checkOutput.Invoke($output, $expected)
+    }
+
+    It 'PowerShell' {
+        $output = pwsh -Command "& {$integrationTestScriptContent}" -ErrorAction Continue
+        $checkOutput.Invoke($output, $expected)
+    }
+}
+
+Describe 'Out-Sentry captures expected stack traces for piped command' {
+    BeforeEach {
+        Push-Location "$PSScriptRoot"
+        $expected = @(
+            '----------------'
+            'AbsolutePath: <No file>'
+            'AddressMode: '
+            'ColumnNumber: '
+            'ContextLine: '
+            'FileName: '
+            'FramesOmitted: '
+            'Function: <ScriptBlock>'
+            'FunctionId: '
+            'ImageAddress: '
+            'InApp: True'
+            'InstructionAddress: '
+            'LineNumber: 3'
+            'Module: '
+            'Package: '
+            'Platform: '
+            'PostContext: '
+            'PreContext: '
+            'SymbolAddress: '
+            'Vars: '
+            '----------------'
+            'AbsolutePath: '
+            'AddressMode: '
+            'ColumnNumber: 5'
+            "ContextLine:     funcA 'throw' 'error'"
+            'FileName: '
+            'FramesOmitted: '
+            'Function: '
+            'FunctionId: '
+            'ImageAddress: '
+            'InApp: True'
+            'InstructionAddress: '
+            'LineNumber: 3'
+            'Module: '
+            'Package: '
+            'Platform: '
+            'PostContext: '
+            'PreContext: '
+            'SymbolAddress: '
+            'Vars: '
+        )
+    }
+
+    AfterEach {
+        Pop-Location
+    }
+
+    It 'Windows PowerShell' -Skip:($env:OS -ne 'Windows_NT') {
+        $output = $integrationTestScriptContent | powershell.exe -Command -
+        $checkOutput.Invoke($output, $expected)
+    }
+
+    It 'PowerShell' {
+        $output = $integrationTestScriptContent | pwsh -Command -
+        $checkOutput.Invoke($output, $expected)
+    }
+}
+
+Describe 'Out-Sentry captures expected stack traces for file input' {
+    BeforeEach {
+        Push-Location "$PSScriptRoot"
+        $expected = @(
+            '----------------'
+            'AbsolutePath: <No file>'
+            'AddressMode: '
+            'ColumnNumber: '
+            'ContextLine: '
+            'FileName: '
+            'FramesOmitted: '
+            'Function: <ScriptBlock>'
+            'FunctionId: '
+            'ImageAddress: '
+            'InApp: True'
+            'InstructionAddress: '
+            'LineNumber: 1'
+            'Module: '
+            'Package: '
+            'Platform: '
+            'PostContext: '
+            'PreContext: '
+            'SymbolAddress: '
+            'Vars: '
+            '----------------'
+            "AbsolutePath: $integrationTestScript"
+            'AddressMode: '
+            'ColumnNumber: 5'
+            "ContextLine:     funcA 'throw' 'error'"
+            'FileName: '
+            'FramesOmitted: '
+            'Function: <ScriptBlock>'
+            'FunctionId: '
+            'ImageAddress: '
+            'InApp: True'
+            'InstructionAddress: '
+            'LineNumber: 14'
+            'Module: '
+            'Package: '
+            'Platform: '
+            'PostContext: }'
+            'catch'
+            '{'
+            '    $_ | Out-Sentry | Out-Null'
+            '}'
+            'PreContext: $transport = [RecordingTransport]::new()'
+            'StartSentryForEventTests ([ref] $events) ([ref] $transport)'
+            'try'
+            '{'
+            'SymbolAddress: '
+            'Vars: '
+        )
+    }
+
+    AfterEach {
+        Pop-Location
+    }
+
+    It 'Windows PowerShell' -Skip:($env:OS -ne 'Windows_NT') {
+        $PSNativeCommandUseErrorActionPreference = $false
+        $output = powershell.exe $integrationTestScript
+        $checkOutput.Invoke($output, $expected)
+    }
+
+    It 'PowerShell' {
+        $PSNativeCommandUseErrorActionPreference = $false
+        $output = pwsh -Command $integrationTestScript
+        $checkOutput.Invoke($output, $expected)
+    }
+}
