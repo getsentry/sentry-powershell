1544d312c89b6314fa94deed5eb4f3af315a95e5
Ivan Dlugos <dlugos.ivan@gmail.com>
2025-02-14 12:24:21 +0100

    chore: update comments

--- modules/Sentry/private/Get-SentryAssembliesDirectory.ps1
+++ modules/Sentry/private/Get-SentryAssembliesDirectory.ps1
@@ -1,12 +1,12 @@
 function GetTFM
 {
-    # Source https://learn.microsoft.com/en-us/powershell/scripting/whats-new/differences-from-windows-powershell?view=powershell-7.4#net-framework-vs-net-core
-    # Also https://learn.microsoft.com/en-us/powershell/scripting/install/powershell-support-lifecycle?view=powershell-7.4#powershell-end-of-support-dates
-    # PowerShell 7.4 (LTS-current) - Built on .NET 8.0
+    # Source https://learn.microsoft.com/en-us/powershell/scripting/install/powershell-support-lifecycle?view=powershell-7.4#powershell-end-of-support-dates
+    # PowerShell 7.5 - Built on .NET 9.0
+    # PowerShell 7.4 (LTS) - Built on .NET 8.0
     # PowerShell 7.3 - Built on .NET 7.0
-    # PowerShell 7.2 (LTS) - Built on .NET 6.0 (LTS-current)
+    # PowerShell 7.2 (LTS) - Built on .NET 6.0
     # PowerShell 7.1 - Built on .NET 5.0
-    # PowerShell 7.0 (LTS) - Built on .NET Core 3.1 (LTS)
+    # PowerShell 7.0 (LTS) - Built on .NET Core 3.1
     # PowerShell 6.2 - Built on .NET Core 2.1
     # PowerShell 6.1 - Built on .NET Core 2.1
     # PowerShell 6.0 - Built on .NET Core 2.0
